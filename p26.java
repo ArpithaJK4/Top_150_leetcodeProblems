68adf3497473f5c23303db1301507849e2a62979
Arpitha JK <150835585+ArpithaJK4@users.noreply.github.com>
2025-01-27 23:43:03 +0530

    Create p26.java

---
+++ p26.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        
+        int n=nums.length;
+        int j=1;
+        for(int i=1;i<n;i++){
+            if(nums[i]!=nums[j-1]){
+                nums[j]=nums[i];
+                j++;
+            }
+        }
+        return j;
+    }
+}
