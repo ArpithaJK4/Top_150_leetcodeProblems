c0792b9b632b6d505f3a2f8278fc8bde58952825
Arpitha JK <150835585+ArpithaJK4@users.noreply.github.com>
2025-03-03 00:15:13 +0530

    Create p283.java

---
+++ p283.java
@@ -0,0 +1,17 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int nonZeroIndex = 0; 
+
+       
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != 0) {
+                nums[nonZeroIndex++] = nums[i];
+            }
+        }
+
+        
+        while (nonZeroIndex < nums.length) {
+            nums[nonZeroIndex++] = 0;
+        }
+    }
+}
