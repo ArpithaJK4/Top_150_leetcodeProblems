3d0e0cc7e1dc37f4a51b2941c427ccbd1cd76f6b
Arpitha JK <150835585+ArpithaJK4@users.noreply.github.com>
2025-03-02 22:36:51 +0530

    Create p268.java

---
+++ p268.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+
+class Solution {
+    public int missingNumber(int[] nums) {
+        int n = nums.length;
+        int expectedSum = n * (n + 1) / 2;
+        int actualSum = 0;
+        
+        for (int num : nums) {
+            actualSum += num;
+        }
+        
+        return expectedSum - actualSum;
+    }
+}
