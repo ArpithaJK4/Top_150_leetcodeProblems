170fd87ace0af04f00883ec498f7c04cf3210642
Arpitha JK <150835585+ArpithaJK4@users.noreply.github.com>
2025-03-17 21:49:41 +0530

    Create P35.java

---
+++ P35.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        int left = 0, right = nums.length - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (nums[mid] == target) {
+                return mid; // Target found
+            } else if (nums[mid] < target) {
+                left = mid + 1; // Search in the right half
+            } else {
+                right = mid - 1; // Search in the left half
+            }
+        }
+        return left; // Position where target should be inserted
+    }
+}
