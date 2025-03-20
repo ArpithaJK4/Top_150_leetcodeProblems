6718d182a0a02ff3bc6a625c8758c9a381ae899f
Arpitha JK <150835585+ArpithaJK4@users.noreply.github.com>
2025-03-20 22:43:58 +0530

    Create P66.java

---
+++ P66.java
@@ -0,0 +1,18 @@
+import java.util.Arrays;
+
+public class Solution {
+    public static int[] plusOne(int[] digits) {
+        int n = digits.length;
+        
+        for (int i = n - 1; i >= 0; i--) {
+            if (digits[i] < 9) {
+                digits[i] += 1;
+                return digits;
+            }
+            digits[i] = 0;
+        }
+        
+        int[] result = new int[n + 1];
+        result[0] = 1; // If all digits were 9, we need an extra digit at the start
+        return result;
+    }}
