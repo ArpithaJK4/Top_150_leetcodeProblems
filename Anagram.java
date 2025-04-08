1ff8cd21436757e125119ad57328f923617aeac3
Arpitha JK <150835585+ArpithaJK4@users.noreply.github.com>
2025-04-08 22:11:07 +0530

    Create Anagram.java

---
+++ Anagram.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        Map<String, List<String>> map = new HashMap<>();
+
+        for (String word : strs) {
+            // Sort the word to create a key
+            char[] chars = word.toCharArray();
+            Arrays.sort(chars);
+            String sortedWord = new String(chars);
+
+            // If the key is not present, add a new list
+            if (!map.containsKey(sortedWord)) {
+                map.put(sortedWord, new ArrayList<>());
+            }
+            
+            // Add the word to the corresponding list
+            map.get(sortedWord).add(word);
+        }
+
+        return new ArrayList<>(map.values());
+    }
+
+    
+}
