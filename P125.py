e94d78824ec4d1834c55521315b30ca002fb1478
Arpitha JK <150835585+ArpithaJK4@users.noreply.github.com>
2025-07-26 22:41:42 +0530

    Update P125.py

--- P125.py
+++ P125.py
@@ -1,5 +1,4 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
          cleaned_s = re.sub(r'[^a-zA-Z0-9]', '', s).lower()
-    # Check if it reads the same forward and backward
-         return cleaned_s == cleaned_s[::-1]
+    return cleaned_s == cleaned_s[::-1]
